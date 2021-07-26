c93cff04e0858f968a1f66337fc3a7d9f8f6c1e9
nitishsainani <nitish.sainani@gmail.com>
2021-07-27 02:21:02 +0530

    new content

--- personal-website/src/content/iAmList.js
+++ personal-website/src/content/iAmList.js
@@ -2,9 +2,8 @@ let getIAmList = () => {
   let iAmLists = [
     {text: 'Howdy!! I am Nitish', delay: 1000},
     {text: 'I build things', delay: 1500},
-    {text: 'Interested in reading? Try this Newsletter Below', delay: 10000},
+    {text: 'Try my Blog Below', delay: 10000},
   ]
-
   for (let i = 0; i < 6; ++i) {
     iAmLists = iAmLists.concat(iAmLists)
   }
