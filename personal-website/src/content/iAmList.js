0d981fb583bf716016c957a01b8047b3fd6f62f3
nitishsainani <nitish.sainani@gmail.com>
2021-09-15 02:06:46 +0530

    changes

--- personal-website/src/content/iAmList.js
+++ personal-website/src/content/iAmList.js
@@ -2,7 +2,7 @@ let getIAmList = () => {
   let iAmLists = [
     {text: 'Howdy!! I am Nitish', delay: 1000},
     {text: 'I build things', delay: 1500},
-    {text: 'Try my Blog Below', delay: 10000},
+    // {text: 'Try my Blog Below', delay: 10000},
   ]
   for (let i = 0; i < 6; ++i) {
     iAmLists = iAmLists.concat(iAmLists)
--- personal-website/src/pages/resumePage.js
+++ personal-website/src/pages/resumePage.js
@@ -28,7 +28,7 @@ function ResumePage(props) {
     <Typography align="center" variant="h4" style={{marginTop: 20, marginBottom: 20}}>Nitish's Resumes</Typography>
     <Divider dark dense elevated/>
     <ResumeCard name={"PDF Document Resume"} buttonIcon="fas fa-arrow-circle-down"
-                onClick={() => window.open("https://docs.google.com/document/u/0/export?format=pdf&id=1uxSu7TjFBTtRt0XIc12fp8cO5T01JKfN9CZrkVc8EP4&token=AC4w5VjDra0ExvyX1Pgume-GvnRldZA0rQ%3A1590599317863&includes_info_params=true&inspectorResult=%7B%22pc%22%3A2%2C%22lplc%22%3A82%7D", "_self")}/>
+                onClick={() => window.open("https://drive.google.com/uc?export=download&id=1DrIwu-yl-4KTLUpt9f8ZLf_-hdLz7biQ", "_self")}/>
     <Divider dark dense elevated/>
     <ResumeCard name={"Audio Resume"} buttonIcon="fas fa-arrow-circle-down" onClick={() => {
     }} disabled/>
