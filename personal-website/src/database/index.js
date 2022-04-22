1c503cbfa75effe61a15baefc3711e7b664b4156
nitishsainani <nitish.sainani@gmail.com>
2022-04-22 19:32:19 +0530

    fix

--- personal-website/src/database/index.js
+++ personal-website/src/database/index.js
@@ -4,7 +4,7 @@ let db = null;
 
 export const getDatabase = async() => {
     if(db) return db;
-    let response = await axios.get('https://nitish.link/static/db.json');
+    let response = await axios.get('https://nitish.club/static/db.json');
     db = response.data;
     return db;
 }
