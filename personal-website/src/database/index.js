284c8ba313becbcc2590d0a9ad8a26e9470bd1b5
nitishsainani <nitish.sainani@gmail.com>
2022-06-26 16:28:28 +0530

    update website to nitish.link YOOOOOO

--- personal-website/src/database/index.js
+++ personal-website/src/database/index.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
 let db = null;
+const baseUrl = window.location.origin.includes("localhost") ? "https://nitish.link" : "https://nitishsainani.github.io"
 
 export const getDatabase = async() => {
     if(db) return db;
-    let response = await axios.get('https://nitishsainani.github.io/static/db.json');
+    let response = await axios.get(`${baseUrl}/static/db.json`);
     db = response.data;
     return db;
 }
--- personal-website/src/pages/resumePage.js
+++ personal-website/src/pages/resumePage.js
@@ -5,38 +5,38 @@ import {Icon} from "../components";
 
 
 function ResumeCard({name, onClick, buttonIcon, disabled}) {
-  return (
-    <Card dark style={{padding: 20, marginLeft: '3vw', marginRight: '3vw', marginTop: 40, marginBottom: 40}}
-          elevation={3}>
-      <Grid container justify="space-between">
-        <Typography align="left" variant="h5" style={{marginTop: 20, marginBottom: 20}}>{name}</Typography>
-        <Card dark style={{height: 80, width: 80, borderRadius: '50%', padding: 11.7}}>
-          <Fab dark size="large" disabled={disabled} onClick={onClick}>
-            <Typography variant="h5">
-              <Icon name={buttonIcon}/>
-            </Typography>
-          </Fab>
+    return (
+        <Card dark style={{padding: 20, marginLeft: '3vw', marginRight: '3vw', marginTop: 40, marginBottom: 40}}
+              elevation={3}>
+            <Grid container justify="space-between">
+                <Typography align="left" variant="h5" style={{marginTop: 20, marginBottom: 20}}>{name}</Typography>
+                <Card dark style={{height: 80, width: 80, borderRadius: '50%', padding: 11.7}}>
+                    <Fab dark size="large" disabled={disabled} onClick={onClick}>
+                        <Typography variant="h5">
+                            <Icon name={buttonIcon}/>
+                        </Typography>
+                    </Fab>
+                </Card>
+            </Grid>
         </Card>
-      </Grid>
-    </Card>
-  )
+    )
 }
 
 
 function ResumePage(props) {
-  return (<>
-    <Typography align="center" variant="h4" style={{marginTop: 20, marginBottom: 20}}>Nitish's Resumes</Typography>
-    <Divider dark dense elevated/>
-    <ResumeCard name={"PDF Document Resume"} buttonIcon="fas fa-arrow-circle-down"
-                onClick={() => window.open("https://drive.google.com/uc?export=download&id=1DrIwu-yl-4KTLUpt9f8ZLf_-hdLz7biQ", "_self")}/>
-    <Divider dark dense elevated/>
-    <ResumeCard name={"Audio Resume"} buttonIcon="fas fa-arrow-circle-down" onClick={() => {
-    }} disabled/>
-    <Divider dark dense elevated/>
-    <ResumeCard name={"Video Resume"} buttonIcon="fas fa-arrow-circle-down" onClick={() => {
-    }} disabled/>
-    <Divider dark dense elevated/>
-  </>);
+    return (<>
+        <Typography align="center" variant="h4" style={{marginTop: 20, marginBottom: 20}}>Nitish's Resumes</Typography>
+        <Divider dark dense elevated/>
+        <ResumeCard name={"PDF Document Resume"} buttonIcon="fas fa-arrow-circle-down"
+                    onClick={() => window.open("https://drive.google.com/uc?export=download&id=1DrIwu-yl-4KTLUpt9f8ZLf_-hdLz7biQ", "_self")}/>
+        <Divider dark dense elevated/>
+        <ResumeCard name={"Audio Resume"} buttonIcon="fas fa-arrow-circle-down" onClick={() => {
+        }} disabled/>
+        <Divider dark dense elevated/>
+        <ResumeCard name={"Video Resume"} buttonIcon="fas fa-arrow-circle-down" onClick={() => {
+        }} disabled/>
+        <Divider dark dense elevated/>
+    </>);
 }
 
 export default ResumePage;
